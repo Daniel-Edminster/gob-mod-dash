7d3f183d9a369f4fe0f479c14ada097142b40e16
RetroTheft <retrotheft@gmail.com>
2021-09-02 12:40:34 +1000

    fix: session persistence

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,5 +1,6 @@
 import snoowrap from 'snoowrap';
 import Reddit from "@/js/classes/Reddit"
+import Snoowrap from 'snoowrap';
 
 const auth = {
    namespaced: true,
@@ -30,13 +31,12 @@ const auth = {
                scope: ['read', 'submit', 'mysubreddits', 'identity'],
                redirectUri: process.env.VUE_APP_REDDIT_REDIRECT_URI,
                permanent: false,
-               state: 'changetorandomstring'
             })
          } catch (err) {
             console.log(err);
          }
       },
-      async instantiateReddit({ commit }, code) {
+      async instantiateReddit({ commit, dispatch }, code) {
          try {
             const instance = await snoowrap.fromAuthCode({
                code,
@@ -44,15 +44,45 @@ const auth = {
                clientId: process.env.VUE_APP_REDDIT_CLIENT_ID_APP,
                redirectUri: process.env.VUE_APP_REDDIT_REDIRECT_URI
             });
-            instance.config({
-               continueAfterRatelimitError: true
+            const reddit = new Reddit(instance);
+            commit('saveReddit', reddit);
+            const username = await reddit.getUsername();
+            commit('saveUsername', username);
+            const session = { accessToken: instance.accessToken, timestamp: Date.now() };
+            dispatch('saveSession', session);
+            return true;
+         } catch (err) {
+            console.log(err);
+         }
+      },
+      saveSession(_, session) {
+         console.log("Storing Session");
+         // localStorage.setItem('mdSession', JSON.stringify(state.reddit));
+         console.log(session);
+         localStorage.setItem('mdSession', JSON.stringify(session));
+      },
+      async checkExistingSession({ dispatch }) {
+         const storedSession = localStorage.mdSession ? JSON.parse(localStorage.mdSession) : null;
+         if (!storedSession) return;
+         const elapsedMin = (Date.now() - storedSession.timestamp) / 60000;
+         if (elapsedMin < 60) {
+            console.log(`Current session will expire in ${Math.floor(60 - elapsedMin)}min.`);
+            await dispatch('refreshReddit', storedSession.accessToken);
+         } else {
+            console.log("Previous session expired. Please login again.");
+            localStorage.removeItem('mdSession');
+         }
+      },
+      async refreshReddit({ commit }, accessToken) {
+         try {
+            const instance = new Snoowrap({
+               userAgent: process.env.VUE_APP_REDDIT_USER_AGENT,
+               accessToken
             });
             const reddit = new Reddit(instance);
-            console.log(reddit);
-            const username = await reddit.getUsername();
             commit('saveReddit', reddit);
+            const username = await reddit.getUsername();
             commit('saveUsername', username);
-            return true;
          } catch (err) {
             console.log(err);
          }
