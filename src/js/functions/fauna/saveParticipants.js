88420ad90eed88cc6e4b4ebce9c510869122ca65
RetroTheft <retrotheft@gmail.com>
2021-09-16 09:53:54 +1000

    fix: cleaned up comments

--- src/js/functions/fauna/saveParticipants.js
+++ src/js/functions/fauna/saveParticipants.js
@@ -1,27 +1,13 @@
 import { client, q } from '@/js/api/fauna'
-// import { addIds } from "./shared"
 
-// Also need the round ID from database to be passed in by this point.
 // Need to separate experience info onto its own object when screening.
 
 export default async function saveSignupsToDatabase(pool, roundNum) {
-   // construct participant documents
    const formattedDocs = pool.map(participant => constructParticipantDocument(participant, roundNum));
-   // save them
-   
-   // might want to delete all participants for that round first
-   // just to be on the safe side
-   const savedDocs = await saveParticipantDocuments(formattedDocs);
+   const savedDocs = await saveParticipantDocuments(roundNum, formattedDocs);
    const usernamesToIds = new Map();
    savedDocs.map(doc => usernamesToIds.set(doc.username, doc.id));
-   
-   // const loadedDocs = await loadParticipantsFromDatabase(roundNum);
-   // console.log(loadedDocs);
-   // rather than add the IDs, better to reload the Participants from the database
-   // since then we can fix the data up and load the username.
-   // add the ids
    const identifiedDocs = addIds(formattedDocs, usernamesToIds);
-   console.log("Identified:", identifiedDocs)
    return identifiedDocs;
 }
 
@@ -43,39 +29,14 @@ function addIds(docs, usernamesToIds) {
    })
 }
 
-// END OF DAY insights:
-// should just return the correct data from here
-// should also maybe just construct the user ref first, but still not sure
-// remember to delete existing first - function is already written
-
-async function saveParticipantDocuments(docs) {
-   const filteredDocs = docs.filter(doc => !doc.id);
+async function saveParticipantDocuments(round, formattedDocs) {
+   const docs = formattedDocs.filter(doc => !doc.id);
    try {
       const response = await client.query(
-         q.Call("register_signups", filteredDocs)
+         q.Call("register_signups", { round, docs })
       )
       return response;
    } catch(err) {
       console.log(err);
    }
 }
-
-// function constructRoundDocument(round) {
-//    const { number, theme, roles } = round;
-//    return {
-//       number,
-//       ...(theme?.id && { theme: q.Ref(q.Collection('themes'), theme.id) }),
-//       ...(roles && { roles })
-//    }
-// }
-
-// async function loadParticipantsFromDatabase(roundNum) {
-//    try {
-//       const response = await client.query(
-//          q.Call("load_participants", roundNum)
-//       )
-//       return response;
-//    } catch (err) {
-//       console.log(err)
-//    }
-// }
