d409fc311f7fdec46eee41b26e109ca0f22c7613
RetroTheft <retrotheft@gmail.com>
2021-09-16 17:21:46 +1000

    fix: Congrats for leaving conditional hell, index!

--- src/components/congrats/CongratsIndex.vue
+++ src/components/congrats/CongratsIndex.vue
@@ -1,21 +1,19 @@
 <template>
-   <div v-if="!post?.source">
-      <PostThread v-if="template" thread="congrats" :metadata="metadata" :forceTemplate="template" />
-      <button v-else @click="constructWinnersThread">Construct Winners Thread</button>
-   </div>
-   <div v-else>
-      <p v-if="!post.id" class="needs-action">Please save the Congrats thread to the database.</p>
-      <p v-else>Congrats thread has been posted and saved! This concludes the round.</p>
-   </div>
+   <button v-if="state === 0" @click="constructWinnersThread">Construct Winners Thread</button>
+   <PostThread v-if="state === 0 && template" thread="congrats" :metadata="metadata" :forceTemplate="template" />
+   <DatasaveWarning v-if="state === 1" thing="Congrats thread" action="ending the round" />
+   <p v-if="state === 2">Congrats thread has been posted and saved! This concludes the round.</p>
 </template>
 
 <script>
 import PostThread from "../shared/PostThread";
+import DatasaveWarning from "../shared/DatasaveWarning"
 import { constructWinnersThread } from "@/js/functions/gob/congrats-reddit";
 
 export default {
    name: "CongratsIndex",
    components: {
+      DatasaveWarning,
       PostThread,
    },
    props: {
@@ -43,6 +41,14 @@ export default {
       };
    },
    inject: ['setProperty'],
+   computed: {
+      state() {
+         let counter = 0; // Construct template, then post or find thread
+         if (this.post?.source) counter++; // Save to database
+         if (this.post?.id) counter++; // That's the end of the round for now.
+         return counter;
+      }
+   },
    methods: {
       constructWinnersThread() {
          const title = `Congratulations to the Winners of Round %num: %theme`;
