bdb6131b6ba18295ee6c87ceb9db1f805b0fb2b1
RetroTheft <retrotheft@gmail.com>
2021-09-20 11:09:42 +1000

    fix: refactor StageWrapper to statelike system

--- src/components/round/StageWrapper.vue
+++ src/components/round/StageWrapper.vue
@@ -1,21 +1,19 @@
 <template>
    <div id="stage-wrapper">
       <ThemeIndex
-         v-if="!round.theme"
+         v-if="stage === 'theme'"
          :metadata="metadata"
          :post="round.threads.theme"
          :nominations="round.pools.theme"
       />
       <SignupIndex
-         v-if="round.theme && !round.participants"
+         v-if="stage === 'signups'"
          :metadata="metadata"
          :post="round.threads.signup"
          :signups="round.pools.signup"
       />
       <TeamIndex
-         v-if="
-            round.participants && !round.songs && !round.active && !round.complete
-         "
+         v-if="stage === 'team'"
          :metadata="metadata"
          :launch="round.threads.launch"
          :participants="round.participants"
@@ -23,7 +21,7 @@
          :active="round.active"
       />
       <LaunchIndex
-         v-if="round.teams && round.active"
+         v-if="stage === 'launch'"
          :metadata="metadata"
          :launch="round.threads.launch"
          :late="round.threads.late"
@@ -32,21 +30,22 @@
          :complete="round.complete"
          :participants="round.participants"
       />
-      <RoundOver v-if="round.complete && !round.songs" :number="round.number" />
+      <RoundOver v-if="stage === 'round-over'" :number="metadata.number" />
       <VoteIndex
-         v-if="!round.active && round.complete && round.songs && !round.winners"
+         v-if="stage === 'voting'"
          :metadata="metadata"
          :post="round.threads.voting"
          :songs="round.songs"
          :votes="round.pools.voting"
       />
       <CongratsIndex
-         v-if="round.winners"
+         v-if="stage === 'congrats'"
          :winners="round.winners"
          :teams="round.teams"
          :metadata="metadata"
          :post="round.threads.congrats"
       />
+      <p v-if="stage === 'wtf'">WTF</p>
    </div>
 </template>
 
@@ -54,24 +53,24 @@
 // this component handles displaying the appropriate component
 // based on what stage the round is at. As it contains a lot of imports
 // and if checks, it's its own component.
-import CongratsIndex from "@/components/congrats/CongratsIndex";
-import LaunchIndex from "@/components/launch/LaunchIndex";
-import TeamIndex from "@/components/team/TeamIndex";
 import ThemeIndex from "@/components/theme/ThemeIndex";
-import RoundOver from "./RoundOver";
 import SignupIndex from "@/components/signup/SignupIndex";
+import TeamIndex from "@/components/team/TeamIndex";
+import LaunchIndex from "@/components/launch/LaunchIndex";
+import RoundOver from "./RoundOver";
 import VoteIndex from "@/components/vote/VoteIndex";
+import CongratsIndex from "@/components/congrats/CongratsIndex";
 
 export default {
    name: "StageWrapper",
    components: {
-      CongratsIndex,
-      LaunchIndex,
-      RoundOver,
-      TeamIndex,
       ThemeIndex,
       SignupIndex,
+      TeamIndex,
+      LaunchIndex,
+      RoundOver,
       VoteIndex,
+      CongratsIndex,
    },
    props: {
       round: {
@@ -80,6 +79,17 @@ export default {
       },
    },
    computed: {
+      stage() { // Yep, this is an absolute mess.
+         const { theme, participants, songs, active, complete, teams, winners} = this.round;
+         if (!theme) return "theme";
+         if (!participants) return "signups";
+         if (!songs && !active && !complete) return "team";
+         if (teams && active) return "launch";
+         if (complete && !songs) return "round-over";
+         if (!active && complete && songs && !winners) return "voting";
+         if (winners) return "congrats";
+         return "wtf";
+      },
       metadata() {
          return {
             number: this.round.number,
