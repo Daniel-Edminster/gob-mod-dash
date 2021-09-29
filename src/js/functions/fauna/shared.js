53cd7e091e55392ab033553e5bfc5b9812849a08
RetroTheft <retrotheft@gmail.com>
2021-09-29 12:13:16 +1000

    feat: improved FaunaDB error handling

--- src/js/functions/fauna/shared.js
+++ src/js/functions/fauna/shared.js
@@ -6,3 +6,22 @@ export function addIds(docs) {
       }
    })
 }
+
+export function isolateFaunaError(errors) {
+   let error;
+   error = errorDive(errors[0]);
+
+   function errorDive(error) {
+      while(error.cause) {
+         error = errorDive(error.cause[0]);
+      }
+      return error;
+   }
+
+   return error;
+}
+
+export function FaunaError(error) {
+   this.name = error.code;
+   this.message = error.description;
+}
